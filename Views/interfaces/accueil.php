fff61c1fc3afaa359e2cd0cc4aa6ae8d4af41719
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-01-16 13:51:36 +0100

    formule (addproduct,updateproduct)

---
+++ Views/interfaces/accueil.php
@@ -0,0 +1,14 @@
+<h3 class="text-center text-secondary mt-3">Bienvenue dans notre platforme !! (👋)</h3>
+
+<div class="container">
+    <div class="card" style="width: 18rem;">
+        <img src="https://th.bing.com/th/id/OIP.vHx3FWMGozzx606qhqghUwHaKc?w=122&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-footer">
+            Card footer
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,7 +7,29 @@ require_once("./Views/includes/header.php");
 
 $home = new HomeController();
 
-$home->index("accueil");
+// $home->index("accueil");
+
+$pages = ['accueil', 'AddProduct', 'DeleteProduct', 'UpdateProduct', 'Dashboard'];
+
+if (isset($_GET['page'])) {
+    if (in_array($_GET['page'], $pages)) {
+        $page = $_GET['page'];
+        if ($page === "Dashboard" || $page === "DeleteProduct" || $page === "UpdateProduct" || $page === "AddProduct") {
+            // if(isset($_SESSION['admin']) && $_SESSION['admin'] === true){
+            $admin = new AdminController();
+            $admin->index($page);
+            // }else{
+            //     include('views/includes/404.php');
+            // }
+        } else {
+            $home->index($page);
+        }
+    } else {
+        // include('views/includes/404.php');
+    }
+} else {
+    $home->index("accueil");
+}
 
 
 
