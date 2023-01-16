c941089bb903fff93a017ce395d00c37e5d5e292
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-01-16 17:21:10 +0100

    page accueil (display product) 👍

--- Views/interfaces/accueil.php
+++ Views/interfaces/accueil.php
@@ -1,14 +1,39 @@
+<?php
+
+
+
+$data = new ProductController();
+$products = $data->getAllProducts();
+// var_dump($products);
+
+
+?>
+
+
+
 <h3 class="text-center text-secondary mt-3">Bienvenue dans notre platforme !! (👋)</h3>
 
 <div class="container">
-    <div class="card" style="width: 18rem;">
-        <img src="https://th.bing.com/th/id/OIP.vHx3FWMGozzx606qhqghUwHaKc?w=122&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        </div>
-        <div class="card-footer">
-            Card footer
+    <div class="row my-5">
+
+        <div class="row">
+            <?php if (count($products)) : ?>
+                <?php foreach ($products as $product) : ?>
+                    <div class="col-md-4 mb-2">
+                        <div class="card " style="width: 18rem;">
+                            <img src="./public/uploads/<?php echo $product["product_image"]; ?>" alt="" class="img-fluid mt-3">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $product["product_title"] ?></h5>
+                                <p class="card-text"><?php echo $product["product_description"] ?></p>
+                            </div>
+                            <div class="card-footer">
+                                <?php echo $product["product_price"] ?> <span style="color: blue;">Dh</span>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </div>
     </div>
+
 </div>
